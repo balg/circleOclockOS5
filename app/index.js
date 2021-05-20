90475a68903a0cd6089884cc250937b0e20fda6f
balg <2700278+balg@users.noreply.github.com>
2021-05-20 17:53:54 +0200

    refactor: Move settings part to index.js

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ import { me } from "appbit";
 import clock from "clock";
 import * as messaging from "messaging";
 import Screen from "./screen";
-import Settings from "./settings";
+import * as fs from "fs";
+import clock from "clock";
 
-const settings = new Settings();
+const SETTINGS_TYPE = "cbor";
+const SETTINGS_FILE = "settings.cbor";
+
 const screen = new Screen();
+const settings = loadSettings();
 
-screen.setColor(settings.get("color"));
+screen.setColor(settings.color);
 
 clock.granularity = "minutes";
 clock.ontick = (evt) => {
@@ -15,13 +19,26 @@ clock.ontick = (evt) => {
 };
 
 // Received message containing settings data
-messaging.peerSocket.addEventListener("message", (evt) => {
-  const { key, value } = evt.data;
-  settings.set(key, value);
-  if (key === "color") {
-    screen.setColor(value);
-  }
+messaging.peerSocket.addEventListener("message", function (evt) {
+  settings[evt.data.key] = evt.data.value;
+  screen.setColor(settings.color);
 });
 
 // Register for the unload event
-me.addEventListener("unload", settings.save);
+me.addEventListener("unload", saveSettings);
+
+// Load settings from filesystem
+function loadSettings() {
+  try {
+    return fs.readFileSync(SETTINGS_FILE, SETTINGS_TYPE);
+  } catch (ex) {
+    return {
+      color: "cyan",
+    };
+  }
+}
+
+// Save settings to the filesystem
+function saveSettings() {
+  fs.writeFileSync(SETTINGS_FILE, settings, SETTINGS_TYPE);
+}
